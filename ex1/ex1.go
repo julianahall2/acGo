1c0cbbca48b0a6282af8ce43f9cb9a5a13bc22ce
Juliana Hall <juju.azeredohall@gmail.com>
2023-08-09 11:58:03 -0300

    ac

--- ex1/ex1.go
+++ ex1/ex1.go
@@ -9,11 +9,12 @@ func main() {
 	fmt.Println("--------------------------")
 	calculaFibo()
 	fmt.Println("--------------------------")
-
+	semana()
+	fmt.Println("--------------------------")
+	e_bissexto(1900)
 }
 
-
-func e_primo () {
+func e_primo() {
 	var n int
 	fmt.Println("Digite um número : ")
 	fmt.Scan(&n)
@@ -35,12 +36,12 @@ func e_primo () {
 }
 
 func calculaFibo() {
-	n := 10
+	n := 5
 	result := fibo(n)
 	fmt.Printf("O %d-ésimo elemento da série de Fibonacci é: %d\n", n, result)
 }
 
-func fibo(n int) int{
+func fibo(n int) int {
 	if n <= 0 {
 		return 0
 	} else if n == 1 || n == 2 {
@@ -60,15 +61,35 @@ func fibo(n int) int{
 }
 
 func semana() {
-	var dia = "segunda"
-	var 
+	var num_dia = 2
 
-	switch dia {
-	case "segunda", "terça","quarta","quinta", "sexta":
-		fmt.Println("Dia de semana")
-	case "sabado", "domingo":
-		fmt.Println("Final de semana")
+	switch num_dia {
+	case 1:
+		fmt.Println("Domingo")
+	case 2:
+		fmt.Println("Segunda")
+	case 3:
+		fmt.Println("Terça")
+	case 4:
+		fmt.Println("Quarta")
+	case 5:
+		fmt.Println("Quinta")
+	case 6:
+		fmt.Println("Sexta")
+	case 7:
+		fmt.Println("Sabado")
 	default:
 		fmt.Println("Erro,dia inválido")
 	}
+}
+
+func e_bissexto(ano int) bool {
+	if ano%4 == 0 {
+		if ano%100 != 0 || ano%400 == 0 {
+			fmt.Println(ano, "é um ano bissexto.")
+			return true
+		}
+	}
+	fmt.Println(ano, "não é um ano bissexto.")
+	return false
 }
