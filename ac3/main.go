00e54ba3368e914e61b1d5d8fa14b9fc311202a0
julianahall2 <julianahall2>
2023-08-23 09:22:49 -0300

    atualizando

--- ac3/main.go
+++ ac3/main.go
@@ -2,8 +2,11 @@ package main
 
 import (
 	"fmt"
+	"bufio"
+	"os"
 	ctt "AC3/contato"
 	"AC3/array"
+	"strings"
 )
 
 func main() {
@@ -17,5 +20,31 @@ func main() {
 
 	fmt.Printf("Novo email:", contato1.Email)
 
+	var contatos [5]*ctt.Contato
+	var nome, email, opcao string
 
+	leitor := bufio.NewReader(os.Stdin)
+
+	fmt.Println("Lista de contatos!")
+	for {
+		fmt.Print("Digite (1) para adicionar, (2) para remover ou qualquer outra coisa para sair: ")
+		fmt.Scanln(&opcao)
+
+		switch opcao {
+		case "1":
+			fmt.Print("Informe o seu nome: ")
+			nome, _ = leitor.ReadString('\n')
+
+			fmt.Print("Informe o seu email: ")
+			fmt.Scanln(&email)
+
+			contatos = array.adicionaContato(ctt.Contato{Nome: nome, Email: email}, contatos)
+		case "2":
+			contatos = array.excluiContato(contatos)
+		default:
+			return
+		}
+
+		fmt.Println(contatos)
+	}
 }
