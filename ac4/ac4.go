3e79889d6a0b9f6faa3bdcff41ac4c7684a6aacf
Juliana Hall <juju.azeredohall@gmail.com>
2023-09-01 19:44:52 -0300

    ac4

--- ac4/ac4.go
+++ ac4/ac4.go
@@ -3,5 +3,19 @@ package main
 import "fmt"
 
 func main() {
-	
+	contador := 0
+	var n int
+	fmt.Scanln(&n)
+	hanoi(n, "A", "B", "C", &contador)
+	fmt.Println("Os discos foram movidos", contador, "vezes")
+}
+
+func hanoi(n int, origem string, destino string, trabalho string, contador *int) {
+
+	if n > 0 {
+		hanoi(n-1, origem, trabalho, destino, contador)
+		fmt.Println("Mova o disco", n, "de", origem, "para", destino)
+		*contador += 1
+		hanoi(n-1, trabalho, destino, origem, contador)
+	}
 }
